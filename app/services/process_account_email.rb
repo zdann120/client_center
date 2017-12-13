834ae2ae97965a8635c248d474c90fb1e82f44c0
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-12-12 23:21:26 -0700

    handle invalid account numbers

--- app/services/process_account_email.rb
+++ app/services/process_account_email.rb
@@ -3,6 +3,8 @@ class ProcessAccountEmail < ActiveInteraction::Base
 
   def execute
     account = Account.find_by_code email_hash['ToFull'][0]['MailboxHash']
+    errors.add(:account, 'not found') unless account
+    return unless account
     account.emails.create!(data: email_hash)
     account
   end
