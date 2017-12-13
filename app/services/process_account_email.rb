9f747a8705faddfdefdb60758d1451a36be0af34
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-12-13 16:23:54 -0700

    upcase account code

--- app/services/process_account_email.rb
+++ app/services/process_account_email.rb
@@ -2,7 +2,7 @@ class ProcessAccountEmail < ActiveInteraction::Base
   hash :email_hash, strip: false
 
   def execute
-    account = Account.find_by_code email_hash['ToFull'][0]['MailboxHash']
+    account = Account.find_by_code email_hash['ToFull'][0]['MailboxHash'].upcase
     errors.add(:account, 'not found') unless account
     return unless account
     email = account.emails.create!(data: email_hash)
